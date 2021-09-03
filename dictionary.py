d2e0a6b4fd838f466c5b1c3649a11827201886b7
HarshitM17 <mathurharshit88@gmail.com>
2021-09-03 11:03:49 +0530

    new

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+dic = {"Name":"Harshit Mathur","D.O.B":"20-07-2001","City":"Jaipur"}
+print(dic)
+print(len(dic))
+print(type(dic))
+
---
+++ var.py
@@ -0,0 +1,6 @@
+a = "Hello,"
+b = " my name is "
+c = "and my age is-> "
+d = input("Name please: ")
+e = input("Age please: ")
+print(a,b,d,c,e)
